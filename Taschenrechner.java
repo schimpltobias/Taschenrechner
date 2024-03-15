8db0a8024b0a814ee2190cf20b85276251e49d4b
schimpltobias <163533378+schimpltobias@users.noreply.github.com>
2024-03-15 21:50:13 +0100

    Add files via upload

---
+++ Taschenrechner.java
@@ -0,0 +1,10 @@
+package test_taschenrechner;
+
+public class Test_Taschenrechner {
+
+    public static void main(String[] args) {
+        Fenster fenster = new Fenster();
+        fenster.setVisible(true);
+    }
+    
+}
